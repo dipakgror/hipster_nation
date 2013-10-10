4b82d22de89aee8922370fee6d1602898cca3220
Dipak <dipakg.ror@gmail.com>
2013-10-10 18:38:49 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HipsterNation::Application.config.secret_token = '89543e9837bf77168575346b585ee6549c1e97205f1a744c4b94e761b5893c029b263a399a4464de9838e398848c2d729abff571a26ef950ebdf124888d724a1'
